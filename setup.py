341128e0859c1a3689bf7dfa617810f0733337cf
bill <billbsing@gmail.com>
2021-02-22 09:34:44 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     tests_require=test_requirements,
     python_requires='>=3.6',
     url='https://github.com/DEX-Company/convex-api-py',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
