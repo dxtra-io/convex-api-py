f7e4aebaef095cfa4701a9a06a2b00a408911070
Isaac Johnston <isaac@superstruct.nz>
2023-05-05 18:36:08 +1200

    Move tests

--- tests/unit/test_key_pair.py
+++ tests/unit/test_key_pair.py
@@ -5,7 +5,7 @@
 """
 import os
 import pytest
-
+import secrets
 
 from convex_api.key_pair import KeyPair
 from tests.types import KeyPairInfo
@@ -13,6 +13,10 @@ from tests.types import KeyPairInfo
 
 SIGN_HASH_TEXT = '5bb1ce718241bfec110552b86bb7cccf0d95b8a5f462fbf6dff7c48543622ba5'
 SIGN_TEXT = '0x7eceffab47295be3891ea745838a99102bfaf525ec43632366c7ec3f54db4822b5d581573aecde94c420554f963baebbf412e4304ad8636886ddfa7b1049f70e'  # noqa: E501
+
+PUBLIC_KEY = '0x5288fec4153b702430771dfac8aed0b21cafca4344dae0d47b97f0bf532b3306'
+PUBLIC_KEY_CHECKSUM = '0x5288Fec4153b702430771DFAC8AeD0B21CAFca4344daE0d47B97F0bf532b3306'
+
 def test_key_pair_create_new():
     key_pair = KeyPair()
     assert(key_pair)
@@ -84,3 +88,23 @@ def test_key_pair_is_equal(test_key_pair: KeyPair):
     assert(not key_pair.is_equal(test_key_pair))
     with pytest.raises(TypeError):
         assert(not key_pair.is_equal(test_key_pair.public_key_bytes)) # type: ignore
+
+def test_key_pair_is_public_key_hex():
+    public_key = secrets.token_hex(32)
+    assert(KeyPair.is_public_key_hex(public_key))
+
+def test_key_pair_is_public_key():
+    public_key = secrets.token_hex(32)
+    assert(KeyPair.is_public_key(public_key))
+
+def test_key_pair_is_public_key_checksum():
+    public_key = secrets.token_hex(32)
+    public_key_checksum = KeyPair.to_public_key_checksum(public_key)
+    assert(KeyPair.is_public_key_checksum(public_key_checksum))
+
+def test_key_pair_to_public_key_checksum():
+    # generate a ethereum public_key
+    # convex public_key to checksum
+    public_key_checksum = KeyPair.to_public_key_checksum(PUBLIC_KEY)
+    assert(KeyPair.is_public_key_checksum(public_key_checksum))
+    assert(public_key_checksum == PUBLIC_KEY_CHECKSUM)
